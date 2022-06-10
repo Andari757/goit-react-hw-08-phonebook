a1cf6f311a8ec401bf7b2e2f58d35c440a08f261
Andari757 <matviyskrypka@gmail.com>
2022-06-10 19:23:54 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -5,55 +5,11 @@ import { actions } from 'redux/slice';
 import ContactList from 'components/ContactList/ContactList';
 import ContactForm from 'components/ContactForm/ContactForm';
 import Filter from "components/Filter/Filter"
+import ContactsPage from 'pages/ContactsPage/ContactsPage';
 export function App() {
-  const [filter, setFilter] = useState("");
-  const items = useSelector(getContacts)
-  const dispatch = useDispatch()
 
-
-  const addContact = (data) => {
-    if (items.find(contact => contact.name === data.name)) {
-      alert(`${data.name} already exists`)
-      return;
-    }
-    const action = actions.addContact(data);
-    dispatch(action);
-  }
-
-  function getFilteredContacts() {
-    if (!filter) {
-      return items;
-    }
-    return items.filter(({ name }) => name.toLowerCase().includes(filter.toLowerCase()))
-  }
-
-  const filterContacts = (e) => {
-    setFilter(e.target.value)
-  }
-
-  const remove = (data) => {
-    const action = actions.removeContact(data);
-    dispatch(action);
-  }
-
-  const data = getFilteredContacts()
   return (
-    <div className="app">
-      <div className="phone-book">
-        <h1>Phonebook</h1>
-        <ContactForm
-          onSubmit={addContact}
-        />
-        <h2>Contacts</h2>
-        <Filter
-          onChange={filterContacts}
-          value={filter} />
-        <ContactList
-          contacts={data}
-          onClick={remove}
-        />
-      </div>
-    </div >
+    <ContactsPage />
   )
 
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { Provider } from 'react-redux';
 import './index.css';
-import { store } from "./redux/store"
+import { store, persistor } from "./redux/store"
+import { PersistGate } from "redux-persist/es/integration/react"
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-
-      <App />
-
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </React.StrictMode >
+  </React.StrictMode>
 );
